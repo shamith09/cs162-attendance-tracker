2008c00cbf2760cab00ac8a211cf0ed4777cbe11
Shamith Pasula <shamith09@berkeley.edu>
2025-01-30 16:03:13 -0800

    db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,31 +1,10 @@
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool(
-  process.env.NODE_ENV === 'production' 
-    ? {
-        host: process.env.MYSQLHOST,
-        port: Number(process.env.MYSQLPORT),
-        user: process.env.MYSQLUSER,
-        password: process.env.MYSQL_ROOT_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-        ssl: {
-          rejectUnauthorized: true
-        },
-        waitForConnections: true,
-        connectionLimit: 5,
-        queueLimit: 0,
-        enableKeepAlive: true,
-        keepAliveInitialDelay: 0
-      }
-    : {
-        socketPath: '/tmp/mysql.sock',
-        user: process.env.MYSQLUSER,
-        password: process.env.MYSQL_ROOT_PASSWORD,
-        database: process.env.MYSQLDATABASE,
-        waitForConnections: true,
-        connectionLimit: 5,
-        queueLimit: 0
-      }
-);
+const pool = mysql.createPool({
+  uri: process.env.MYSQL_PUBLIC_URL,
+  waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0
+});
 
 export default pool; 
