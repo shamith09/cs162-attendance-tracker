ee401dfbd627d26ecbfc6da0ae7ba02d8dd8feeb
Shamith Pasula <shamith09@berkeley.edu>
2025-02-04 19:56:12 -0800

    dependency fix

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Config options here
-};
-
-const withVercelToolbar = require('@vercel/toolbar/plugins/next')();
-
-module.exports = withVercelToolbar(nextConfig); 
